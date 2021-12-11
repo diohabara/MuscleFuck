a16ab0a5a9bef8d5d9eb5938a5afa32ade1a0a97
diohabara <diohabara@gmail.com>
2021-12-11 12:14:53 +0900

    fix: make it work

--- src/main.py
+++ src/main.py
@@ -2,10 +2,18 @@ import time
 from typing import List
 
 from gpiozero import Button
+from singal import pause
+
 from muscle_fuck import MuscleFuck
 
 
 def main() -> None:
+    mf = MuscleFuck()
+    program: List[str] = []
+    is_running = False
+    start: float
+    time_list: List[float] = []
+
     def display_time() -> None:
         if not time_list:
             print("Welcome to this contest")
@@ -14,7 +22,52 @@ def main() -> None:
             for i, t in enumerate(time_list):
                 print(f"{t} ranked in {i+1}")
 
-    print("Start")
+    def button1_pressed() -> None:
+        print("1")
+        program.append(">")
+
+    def button2_pressed() -> None:
+        print("2")
+        program.append("<")
+
+    def button3_pressed() -> None:
+        print("3")
+        program.append("+")
+
+    def button4_pressed() -> None:
+        print("4")
+        program.append("-")
+
+    def button5_pressed() -> None:
+        print("5")
+        program.append(".")
+
+    def button6_pressed() -> None:
+        print("6")
+        program.append(",")
+
+    def button7_pressed() -> None:
+        print("7")
+        program.append("[")
+
+    def button8_pressed() -> None:
+        print("8")
+        program.append("]")
+
+    def button9_pressed() -> None:
+        nonlocal start
+        nonlocal program
+        nonlocal is_running
+        if is_running:
+            display_time()
+            start = time.time()
+        else:
+            time_list.append(time.time() - start)
+            start = 0.0
+            mf.run_from_program("".join(program))
+            program = []
+        is_running = not is_running
+
     button1 = Button(5)
     button2 = Button(6)
     button3 = Button(13)
@@ -25,47 +78,17 @@ def main() -> None:
     button8 = Button(20)
     button9 = Button(21)
 
-    mf = MuscleFuck()
-    program: List[str] = []
-    is_running = False
-    start: float
-    time_list: List[float] = []
-
-    while True:
-        if button9.is_pressed:
-            if is_running:
-                display_time()
-                start = time.time()
-            else:
-                time_list.append(time.time() - start)
-                start = 0.0
-                mf.run_from_program("".join(program))
-                program = []
-            is_running = not is_running
-        if button1.is_pressed:
-            print("1")
-            program.append(">")
-        if button2.is_pressed:
-            print("2")
-            program.append("<")
-        if button3.is_pressed:
-            print("3")
-            program.append("+")
-        if button4.is_pressed:
-            print("4")
-            program.append("-")
-        if button5.is_pressed:
-            print("5")
-            program.append(".")
-        if button6.is_pressed:
-            print("6")
-            program.append(",")
-        if button7.is_pressed:
-            print("7")
-            program.append("[")
-        if button8.is_pressed:
-            print("8")
-            program.append("]")
+    button1.is_pressed = button1_pressed
+    button2.is_pressed = button2_pressed
+    button3.is_pressed = button3_pressed
+    button4.is_pressed = button4_pressed
+    button5.is_pressed = button5_pressed
+    button6.is_pressed = button6_pressed
+    button7.is_pressed = button7_pressed
+    button8.is_pressed = button8_pressed
+    button9.is_pressed = button9_pressed
+    print("Start")
+    pause()
 
 
 if __name__ == "__main__":
